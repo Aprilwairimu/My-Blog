1c8ec4efc08ac9df1290bf423b1d45c0013ea013
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-13 21:15:49 +0300

    created an app instance

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+class Config:    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
+    
+class ProdConfig(Config):
+    pass
+class TestConfig(Config):
+    
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
