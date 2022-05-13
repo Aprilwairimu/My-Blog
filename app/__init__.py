a338007d665387fd0d31fbaaf176c4d17af256be
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-13 22:55:16 +0300

    installed email-validator

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from config import config_options
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-# from flask_mail import Mail
-from flask_bootstrap import Bootstrap
-# from .models import User
 
-# db = SQLAlchemy()
-# mail = Mail()
+from flask_login import LoginManager
+
+from flask_bootstrap import Bootstrap
+
+
 bootstrap = Bootstrap()
 
 login_manager = LoginManager()
@@ -18,20 +16,20 @@ def create_app(config_name):
     app = Flask(__name__)
 
     #Initializing Flask Extensions
-    # db.init_app(app) 
+   
     login_manager.init_app(app) 
     bootstrap.init_app(app)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-   # Registering the blueprint
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
+    #Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
     return app
