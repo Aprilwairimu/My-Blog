9f54ebd62ed8ab572459dde829abe3f5be874ac5
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-13 21:56:10 +0300

    added index and macros.html

--- manage.py
+++ manage.py
@@ -27,4 +27,4 @@ manager.add_command('server',Server)
 #     return dict(app = app, db = db,User = User, Pitch = Pitch,Comment=Comment)
 
 if __name__ == '__main__':
-    manager.run(debug=True)
+    manager.run()
