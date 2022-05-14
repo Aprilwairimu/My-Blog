0adbcb3509d7f8d44136b1f046fe4a6351075fed
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-14 15:25:43 +0300

    added a class profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,7 @@ from wtforms.validators import InputRequired
 class CommentForm(FlaskForm):
 	description = TextAreaField('Add comment',validators=[InputRequired()])
 	submit = SubmitField()
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,12 @@ from flask.views import View,MethodView
 def home():
     return render_template('home.html')
 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
