e825952ade40267f9677e648bb19e7d1525e4a2d
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-14 11:17:54 +0300

    installed flask-SQLALCHEMY

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from config import config_options
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 from flask_bootstrap import Bootstrap
 
-
+db = SQLAlchemy()
 bootstrap = Bootstrap()
 
 login_manager = LoginManager()
@@ -16,7 +16,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     #Initializing Flask Extensions
-   
+    db.init_app(app) 
     login_manager.init_app(app) 
     bootstrap.init_app(app)
 
---
+++ app/main/views.py
@@ -0,0 +1,8 @@
+from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort,flash
+from . import main
+from flask.views import View,MethodView
+
+@main.route('/', methods = ['GET','POST'])
+def home():
+    return render_template('home.html')
