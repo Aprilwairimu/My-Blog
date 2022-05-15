df6536348bbe904c5fdff6f87405144b955b7288
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-15 20:16:46 +0300

    installed flask-migrate

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 class Config:   
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     QUOTE_KEY = 'http://quotes.stormconsultancy.co.uk/random.json'
     
     
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
 class TestConfig(Config):
     
     pass
